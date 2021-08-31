080d29899655e55b177ac8dbb0874e11d184b006
parhamsaremi <pa.sa.13792000@outlook.com>
2021-08-31 23:30:41 +0430

    fixed simplemodel

--- models/simplemodel.py
+++ models/simplemodel.py
@@ -6,10 +6,20 @@ class SimpleModel(nn.Module):
       ## base model ###################################################################
       self.encoder_hidden_layer = nn.Linear(in_features=input_shape, out_features=embedding_size)
       self.encoder_output_layer = nn.Linear(in_features=embedding_size, out_features=embedding_size)
+      self.relu = nn.ReLU()
 
       ## mlp projection head ##########################################################
       self.backbone = nn.Sequential(nn.Linear(embedding_size, embedding_size),
-                                       nn.ReLU(), self.encoder_output_layer , self.encoder_hidden_layer)
+                                       nn.ReLU())
 
     def forward(self , features):
-      return self.backbone(features)
+      l1 = self.encoder_hidden_layer(features)
+      l2 = self.encoder_output_layer(l1)
+      l3 = self.relu(l2)
+      return self.backbone(l3)
+    def forward(self , features):
+      l1 = self.encoder_hidden_layer(features)
+      l2 = self.encoder_output_layer(l1)
+      l3 = self.relu(l2)
+      return l3
+
